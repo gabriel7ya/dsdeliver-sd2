ce7a4b3f72e7e2795817a049844f9ac89e6355a6
gabriel7ya <gabriel7ya@gmail.com>
2021-01-17 22:30:41 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,9 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
-import Header from './src/Header';
 import AppLoading from 'expo-app-loading';
+import Routes from './src/Routes';
+import Header from './src/Header';
 import Home from './src/Home';
 
 export default function App() {
@@ -20,8 +21,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
-export default function Home() {
-    const handleOnPress = () => {
-        
-    }
+function Home() {
+
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+      navigation.navigate('Orders');
+  }
 
   return (
     <>
+    <Header />
     <View style={styles.container}>
       <Image source={require('../assets/deliveryman.png')} />
       <Text style={styles.title}>
@@ -70,3 +75,5 @@ const styles = StyleSheet.create({
       letterSpacing: -0.24
     }
   });
+
+export default Home;
